b04fc11102105a372280583f80fdc59c652ed159
LuigginaRossi <luigginavaldivia@gmail.com>
2023-02-17 18:04:13 +0100

    edited pages

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -26,27 +26,27 @@
 
 <template>
     <h1 >Projects Page</h1>
-
-    <div class="row row-cols-3 row-cols-md-4 g-4">
-     <div class="col"  v-for="project in projects" :key="project.id">
-        <!-- <div v-for="El in project" :key="El.id"> -->
-                <div class="card">
-                    <div v-if="project.cover_img">
-                        <img :src="`${backendUrl}/storage/${project.cover_img}`" class="card-img-top" alt="...">
-                    </div> 
-                    
-                    <div class="card-body">
-                        <h5 class="card-title">{{ project.name }}</h5>
-                        <p class="card-text">{{ project.description }}</p>
-                        <p class="card-text">{{ project.type?.name}}</p>
-                        <p v-for="technology in project.technologies" :key="technology.id">
-                           TECNOLOGY: {{ technology.name }}
-                        </p>
+    <div class="container">
+        <div class="row row-cols-3 row-cols-md-4 g-4">
+            <div class="col"  v-for="project in projects" :key="project.id">
+                <!-- <div v-for="El in project" :key="El.id"> -->
+                    <div class="card">
+                        <div v-if="project.cover_img">
+                            <img :src="`${backendUrl}/storage/${project.cover_img}`" class="card-img-top" alt="...">
+                        </div> 
+                        
+                        <div class="card-body">
+                            <h5 class="card-title">{{ project.name }}</h5>
+                            <p class="card-text">{{ project.description }}</p>
+                            <p class="card-text">{{ project.type?.name }}</p>
+                            
+                        </div>
+                        
+                        <router-link  class="btn btn-primary" :to="{ name: 'project', params: { id: project.id } }"> More Info </router-link>
                     </div>
-                    
-                    <router-link  class="btn btn-primary" :to="{name: 'project', params:{ id: project.id }}"> More Info </router-link>
-                </div>
-          </div>
-        <!-- </div> -->
+              </div>
+            <!-- </div> -->
+        </div>
     </div>
+    
 </template>
--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -26,19 +26,26 @@ import axios from 'axios';
 
 <template >
     <h1>Single Progect Page</h1>
-    <div class="col">
-        <div class="card">
-        <div v-if="project.cover_img">
-            <img :src="`${backendUrl}/storage/${project.cover_img}`" class="card-img-top" alt="...">
-        </div> 
-        <div class="card-body">
-            <h5 class="card-title">{{ project.name }}</h5>
-            <p class="card-text">{{ project.description }}</p>
-            <!-- <p class="card-text">{{ project.type?.name}}</p> -->
-
+    <div class="container">
+        <div class="row">
+            <div class="col d-flex justify-content-center pb-5">
+                <div class="card  w-50">
+                <div v-if="project.cover_img">
+                    <img :src="`${backendUrl}/storage/${project.cover_img}`" class="card-img-top" alt="...">
+                </div> 
+                <div class="card-body">
+                    <h5 class="card-title">{{ project.name }}</h5>
+                    <p class="card-text">{{ project.description }}</p>
+                    <!-- <p class="card-text">{{ project.type?.name}}</p> -->
+                    <p  class="card-text" v-for="technology in project.technologies" :key="technology.id">
+                       TECNOLOGY: {{ technology?.name }}
+                    </p>
+                    <p>TIPO: {{ project.type?.name }}</p>
+                </div>
+               
+                 <!-- <router-link :to="{name: 'project', params:{id: 'project.id'}}"> More Info</router-link>  -->
+            </div>
         </div>
-       
-         <!-- <router-link :to="{name: 'project', params:{id: 'project.id'}}"> More Info</router-link>  -->
     </div>
     </div>
     
